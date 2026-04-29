f471c88b3f757d64efba5466f7f59d3510ba5f57
kl3inIT <dathip04@gmail.com>
2026-04-30 00:29:50 +0700

    fix: keep worker service running

--- backend/worker/build.gradle.kts
+++ backend/worker/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
     implementation(project(":backend:core"))
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
